6a5d5494fb2ccea859ceb9aa44cab50ab799ee50
soroshahmadi <soroshahmadi@outlook.com>
2017-02-13 14:46:21 -0800

    My first commit

--- SwiftAppLabels/ViewController.swift
+++ SwiftAppLabels/ViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolLabel: UILabel!
+  
+    var tapCount = 0//everytime the tap it, then tap count will be equal to whatever it is + 1//tapCount is the variable
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        tapCount = tapCount + 1 //whenever button is pressed it will show the tap count
+        
+        if tapCount >= 10 {//if tapCount is pressed greater than or equal to 10 times...
+            coolLabel.text = "You tapped the button 10 times!"//then the coolLabel text will say how it was pressed 10 times
+        }
+       
+    }
+    
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        coolLabel.text = "Buttons are Cool!"
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
