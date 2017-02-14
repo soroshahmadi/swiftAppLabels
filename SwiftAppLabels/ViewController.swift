514a2c8303c4e71fd3a445fc414808b9c91170aa
soroshahmadi <soroshahmadi@outlook.com>
2017-02-13 16:40:15 -0800

    Now we can add numbers

--- SwiftAppLabels/ViewController.swift
+++ SwiftAppLabels/ViewController.swift
@@ -10,38 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    
+    
+    
     @IBOutlet weak var coolLabel: UILabel!
   
     var tapCount = 0//everytime the tap it, then tap count will be equal to whatever it is + 1//tapCount is the variable
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        tapCount = tapCount + 1 //whenever button is pressed it will show the tap count
-        
-        if tapCount >= 20 {//if tapCount is pressed greater than or equal to 10 times...
-            coolLabel.text = "You tapped the button 20 times!"//then the coolLabel text will say how it was pressed 10 times
+       coolLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+  
         }
        
     }
     
     
-    @IBAction func buttonPressed(_ sender: Any) {
-        coolLabel.text = "Buttons are Cool!"
+
     
-    }
+
+
+        
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
-}
+
 
